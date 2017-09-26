a1ca717b5b6d2da742e6978e6740ae885b7c5a22
Myheart1 <31686574+Myheart1@users.noreply.github.com>
2017-09-26 09:33:19 +0800

    Add files via upload

--- 1.cpp
+++ 1.cpp
@@ -1,79 +1,137 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<windows.h>
+#include <time.h> 
 void main()
 {
-    int b=0,c,d=0,e,f;
-    int x,right=0,wrong=0;
-    while(1)
-    {
-    x=rand()%4;
-    c=rand()%100;
-    e=rand()%100;
-        switch(x)
-        {
-        case 0:
-            printf("%d + %d =",c,e);
-            break;
-        case 1:
-            printf("%d - %d =",c,e);
-            break;
-        case 2:
-            printf("%d * %d =",c,e);
-            break;
-        case 3:
-            printf("%d ÷ %d =",c,e);
-            break;
-        }
-        scanf("%d",&f);
-        switch(x)
-        {
-        case 0:
-            if(f==c+e)
-            {
-                printf("计算正确!\n");right++;
-            }
-            else
-            {
-                printf("计算错误!\n");wrong++;
-            }
-            break;
-        case 1:
-            if(f==c-e)
-            {
-                printf("计算正确!\n");right++;
-            }
-            else
-            {
-                printf("计算错误!\n");wrong++;
-            }
-            break;
-        case 2:
-            if(f==c*e)
-            {
-                printf("计算正确!\n");right++;
-            }
-            else
-            {
-                printf("计算错误!\n");wrong++;
-            }
-            break;
-        case 3:
-            if(f==c/e)
-            {
-                printf("计算正确!\n");right++;
-            }
-            else
-            {
-                printf("计算错误!\n");wrong++;
-            }
-            break;
-        }
-        printf("按任意键继续，q则退出!\n");
-    //    scanf("%d",&quit);
-    //    if(quit==0)
-        fflush(stdin);
-        if(getchar()=='q')
-            break;
-    }
-    printf("共答对%d道题，共答错%d道题\n",right,wrong);
+	int a,b,i=20,j=0,x,f,h,right=0,wrong=0,s;
+	float c,d,k;
+    srand( (unsigned)time( NULL ) );
+	printf("【-----------四则运算-----------】\n");
+	printf("欢迎进入初级阶段 \n");
+	do
+	
+	{ 
+		
+		a=rand()%100+1;
+		b=rand()%100+1;
+		x=rand()%4;
+		switch(x)
+		{
+			
+		case 0:
+			printf("%d+%d=",a,b);
+			scanf("%d",&f);
+			if(f==a+b)
+			{
+				printf("计算正确!\n");right++;
+			}
+			else
+			{
+				printf("计算错误!\n");wrong++;
+			}
+			break;
+			
+		case 1:
+			printf("%d-%d=",a,b);
+			scanf("%d",&f);
+			if(f==a-b)
+			{
+				printf("计算正确!\n");right++;
+			}
+			else
+			{
+				printf("计算错误!\n");wrong++;
+			}
+			break;
+		case 2:
+			printf("%d*%d=",a,b);
+			scanf("%d",&f);
+			if(f==a*b)
+			{
+				printf("计算正确!\n");right++;
+			}
+			else
+			{
+				printf("计算错误!\n");wrong++;
+			}
+			break;
+		case 3:
+			printf("%d/%d=",a,b);
+			scanf("%d",&f);
+			if(f==a/b)
+			{
+				printf("计算正确!\n");right++;
+			}
+			else
+			{
+				printf("计算错误!\n");wrong++;
+			}
+			break;
+		}
+		
+	}
+	while(right!=i);
+	
+	printf("恭喜你正确完成20道初级题目！你想要完成更高难度的算术题吗？y/n \n");
+	scanf("%d",&s);
+	if(getchar()=='y')
+	{
+		printf("你想要挑战多少道题：");
+		scanf("%d",&h);
+		for(j=0;j<h;j++)
+		{
+			c=float(rand()*0.001+1);
+			d=float(rand()*0.001+1);
+			x=rand()%4;
+			
+			switch(x)
+			{
+			case 0:printf("%.3f+%.3f=\n",c,d);
+				
+				
+				k=c+d;
+				k=int(k*100+0.5)/100.0;
+				Sleep (6000);
+				printf("%.2f\n",k);
+				break;
+			case 1:printf("%.3f-%.3f=\n",c,d);
+				k=c-d;
+				if(k>0)
+				{
+					k = int(k*100+0.5)/100.0;
+				}
+				else
+				{
+					k = int(k*100-0.5)/100.0;
+				}
+			Sleep (6000);
+				
+				printf("%.2f\n",k);break;
+			case 2:printf("%.3f*%.3f=\n",c,d);
+				k=c*d;
+				k=int(k*100+0.5)/100.0;
+				Sleep (6000);
+				printf("%.2f\n",k);
+				break;
+				
+			case 3:printf("%.3f/%.3f=\n",c,d);
+				k=c/d;
+				k=int(k*100+0.5)/100.0;
+				Sleep (6000);
+				
+				printf("%.2f\n",k);break;
+				
+				
+				
+			}
+
+		}
+			printf("你真棒！\n");
+	}
+	
+	else{
+		printf("期待你的下次访问！\n");
+	}
+	
 }
